8580ec232ff90810cf4183ea0e20f65baab36dc3
greyoxide <greyoxide@gmail.com>
2019-08-28 17:28:31 -0700

    forgot to set host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'talk-scheduler.herokuapp.com' }
 
   config.action_mailer.default_options = {from: 'no-reply.winchester.cong'}
   config.action_mailer.smtp_settings = {
